033f1450fb1efb1055d32de9298efe71a62292f6
Cansero <131320384+Cansero@users.noreply.github.com>
2023-10-11 22:24:40 -0400

    Changes

--- Main.gd
+++ Main.gd
@@ -6,16 +6,24 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
@@ -40,7 +48,3 @@ func _on_mob_timer_timeout():
 	mob.linear_velocity = velocity.rotated(direction)
 	
 	add_child(mob)
-
-
-func _ready():
-	new_game()
